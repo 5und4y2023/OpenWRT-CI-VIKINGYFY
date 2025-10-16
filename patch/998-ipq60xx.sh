ab10e8cb9cd5452d666b7c7c96be38fa65551a87
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-10-16 16:32:24 +0800

    Create 998-ipq60xx.sh

---
+++ patch/998-ipq60xx.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.51-1-aca694a05b8cf25863680b90251e8810/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+# wifi设置
+# mymac=$(ifconfig br-lan 2>/dev/null | awk '/HWaddr/ {print $5}' | awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+# uci set wireless.default_radio0.ssid=WiFi-${mymac}-5G
+# uci set wireless.default_radio1.ssid=WiFi-${mymac}-2.4G
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci commit wireless
+
+#uci set network.lan.ipaddr='192.168.1.1'
+#uci set network.usbwan=interface
+#uci set network.usbwan.proto='dhcp'
+#uci set network.usbwan.device='eth0'
+#uci add_list firewall.cfg03dc81.network='usbwan'
+#uci commit network
+uci commit
+cp /etc/my-clash /etc/openclash/core/clash_meta
+#不用重启network，源码自带
+exit 0
