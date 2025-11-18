8f140a9b0fcce73080b3043e1ddac67d5a70c128
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-18 21:49:43 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -11,11 +11,11 @@ else
 	mv $GITHUB_WORKSPACE/patch/998-$IPQ_TARGET.sh package/base-files/files/etc/uci-defaults/998-ipq.sh
 fi
 
-mv $GITHUB_WORKSPACE/patch/xy/footer.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer.ut
-mv $GITHUB_WORKSPACE/patch/xy/footer_login.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
-mv $GITHUB_WORKSPACE/patch/xy/997-mac2.sh package/base-files/files/etc/uci-defaults/997-mac2.sh
-mv $GITHUB_WORKSPACE/patch/xy/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-mv $GITHUB_WORKSPACE/patch/xy/998-istore.sh package/base-files/files/etc/uci-defaults/998-ipq.sh
+#mv $GITHUB_WORKSPACE/patch/xy/footer.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer.ut
+#mv $GITHUB_WORKSPACE/patch/xy/footer_login.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
+#mv $GITHUB_WORKSPACE/patch/xy/997-mac2.sh package/base-files/files/etc/uci-defaults/997-mac2.sh
+#mv $GITHUB_WORKSPACE/patch/xy/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+#mv $GITHUB_WORKSPACE/patch/xy/998-istore.sh package/base-files/files/etc/uci-defaults/998-ipq.sh
 
 
 rm -rf feeds/packages/lang/golang
