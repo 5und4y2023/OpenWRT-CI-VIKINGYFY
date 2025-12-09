1d44a1d78aac52b9465ef8ace89f516b1c2b97d4
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-12-09 17:03:56 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -80,6 +80,9 @@ fi
 #sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+#修改版本号
+sed -i "s/%V/24.10/g" package/base-files/files/usr/lib/os-release
+sed -i "s/%V/24.10/g" package/base-files/files/etc/openwrt_release
 #添加编译日期标识
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/usr/lib/os-release
