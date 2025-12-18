3d85d267f480a35a0fdd89e45fbb4cb7b3ee3fc5
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-12-18 18:21:34 +0800

    Update 998-istore-ipq60xx.sh

--- patch/998-istore-ipq60xx.sh
+++ patch/998-istore-ipq60xx.sh
@@ -15,8 +15,8 @@ sed -i '$a src/gz openwrt_telephony https://mirrors.pku.edu.cn/openwrt/releases/
 echo > /etc/opkg/customfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/releases/24.10/packages/aarch64_cortex-a53/kiddin9' /etc/opkg/customfeeds.conf
 
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -27,21 +27,21 @@ uci set dropbear.@dropbear[0].Interface=''
 # wifi设置
 uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
 #uci set wireless.default_radio0.ssid=OpenWrt-5G
-uci set wireless.radio0.txpower='20'
+#uci set wireless.radio0.txpower='20'
 uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-uci set wireless.radio1.txpower='20'
+#uci set wireless.radio1.txpower='20'
 
-uci set wireless.default_radio0.key=password
-uci set wireless.default_radio1.key=password
-uci commit wireless
+#uci set wireless.default_radio0.key=password
+#uci set wireless.default_radio1.key=password
+#uci commit wireless
 
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
 #uci set network.usbwan.device='eth0'
 #uci add_list firewall.cfg03dc81.network='usbwan'
 
-uci set network.lan.ipaddr=192.168.3.1
-uci commit network
+#uci set network.lan.ipaddr=192.168.3.1
+#uci commit network
 uci commit
 
 #不用重启network，源码自带
