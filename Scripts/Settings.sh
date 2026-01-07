6aa1619a0264d7eac74ea0dbb4fd7bea4c8f3b36
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-01-07 15:22:14 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,14 +49,6 @@ git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/
 rm -rf feeds/packages/net/{open-app-filter}
 git clone --depth 1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
 
-echo >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update nas nas_luci istore
-./scripts/feeds install -a -p nas
-./scripts/feeds install -a -p nas_luci
-./scripts/feeds install -d y -p istore luci-app-store
 
 sed -i '129d' include/kernel-defaults.mk
 sed -i '129i\\tcp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
