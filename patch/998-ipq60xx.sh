6f2eea9faee4ba540967540a7f264cef93ba1533
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-04-07 20:02:14 +0800

    Update 998-ipq60xx.sh

--- patch/998-ipq60xx.sh
+++ patch/998-ipq60xx.sh
@@ -83,7 +83,7 @@ sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/video/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.vsean.net/openwrt#g' /etc/apk/repositories.d/distfeeds.list
-#sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.77-1-ddaf4cbd24e7b8dd27fd42054531a6f8/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.77-1-ddaf4cbd24e7b8dd27fd42054531a6f8/packages.adb' /etc/apk/repositories.d/distfeeds.list
 sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
 
 # 设置所有网口可访问网页终端
@@ -94,13 +94,14 @@ uci set dropbear.@dropbear[0].Interface=''
 
 
 # wifi设置
-uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')
 #if uci -q get wireless.default_radio2 >/dev/null; then
 #    uci set wireless.default_radio2.ssid='WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G2'
 #fi
 #uci set wireless.radio0.txpower='20'
-uci set wireless.default_radio0.encryption='psk2+ccmp'
 uci set wireless.default_radio0.key='1234qwer+-'
+uci set wireless.default_radio1.key='1234qwer+-'
 uci commit wireless
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
