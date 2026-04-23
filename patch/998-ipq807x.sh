0e7a116fa6c2187661b556eb74a94c3461d7458d
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-04-23 21:21:12 +0800

    Update 998-ipq807x.sh

--- patch/998-ipq807x.sh
+++ patch/998-ipq807x.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+#uci set luci.main.mediaurlbase='/luci-static/bootstrap'
+#uci commit luci
 
 sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/video/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.vsean.net/openwrt#g' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq807x/kmods/6.12.74-1-89368b630946e2107fbdc359c30a3e9a/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/kmods/6.12.80-1-044bf655d806999739e8539fcbad05cd/packages.adb' /etc/apk/repositories.d/distfeeds.list
 sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq807x/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
 
 # 设置所有网口可访问网页终端
@@ -14,20 +17,27 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-# wifi设置
-uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
-#uci set wireless.radio0.txpower='20'
-uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-#uci set wireless.radio1.txpower='20'
-uci commit wireless
 
-#uci set network.lan.ipaddr='192.168.1.1'
+# wifi设置
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-A
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-B
+if uci -q get wireless.default_radio2 >/dev/null; then
+    uci set wireless.default_radio2.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-C
+    uci set wireless.radio0.channel='149'
+    uci set wireless.default_radio2.key='123456qwerty'
+fi
+#uci set wireless.radio0.txpower='20'
+uci set wireless.default_radio0.key='123456qwerty'
+uci set wireless.default_radio1.key='123456qwerty'
+uci commit wireless
 #uci set network.usbwan=interface
 #uci set network.usbwan.proto='dhcp'
 #uci set network.usbwan.device='eth0'
-#uci add_list firewall.cfg03dc81.network='usbwan'
-#uci commit network
-uci commit
 
+
+uci commit dhcp
+uci commit network
+uci commit wireless
+uci commit
 #不用重启network，源码自带
 exit 0
