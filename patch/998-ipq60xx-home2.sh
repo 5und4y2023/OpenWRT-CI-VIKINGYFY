6b3019213bf5a7f01cd118aa894e267d43ba8c3b
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-05-02 16:14:21 +0800

    Create 998-ipq60xx-home2.sh

---
+++ patch/998-ipq60xx-home2.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+uci add network device
+uci set network.@device[-1].type='bridge'
+uci set network.@device[-1].name='m-wan'
+uci add_list network.@device[-1].ports='wan'
+
+uci add network bridge-vlan
+uci set network.@bridge-vlan[-1].device='m-wan'
+uci set network.@bridge-vlan[-1].vlan='1'
+uci add_list network.@bridge-vlan[-1].ports='wan:u*'
+#uci add_list network.@bridge-vlan[-1].ports='wan'
+
+uci add network bridge-vlan
+uci set network.@bridge-vlan[-1].device='m-wan'
+uci set network.@bridge-vlan[-1].vlan='441'
+uci add_list network.@bridge-vlan[-1].ports='wan:t'
+
+uci del network.@device[0].ports
+uci add_list network.@device[0].ports='lan1'
+uci add_list network.@device[0].ports='lan2'
+uci add_list network.@device[0].ports='lan3'
+uci add_list network.@device[0].ports='lan4'
+uci add_list network.@device[0].ports='m-wan.1'
+
+uci set network.wan.device='m-wan.441'
+uci commit network
+
+uci set luci.main.mediaurlbase='/luci-static/bootstrap'
+uci commit luci
+
+
+uci set wireless.default_radio0.ssid=@Ruijie-s227B_5G
+uci set wireless.radio0.txpower='20'
+uci set wireless.default_radio1.ssid=@Ruijie-s227B
+uci set wireless.radio1.txpower='20'
+uci set wireless.default_radio0.key=86880959189
+uci set wireless.default_radio1.key=86880959189
+uci commit wireless
+
+uci del network.lan.ipaddr
+uci add_list network.lan.ipaddr='192.168.13.1/24'
+uci del network.wan6
+uci set network.lan.ip6assign=64
+uci del network.globals.ula_prefix
+uci set network.lan.ip6ifaceid='random'
+
+uci set network.wan.proto='pppoe'
+uci set network.wan.username='037606332332'
+uci set network.wan.password='332332'
+
+uci commit dhcp
+uci commit network
+uci commit
+
+sed -i 's/root::0:0:99999:7:::/root:$1$uqDPdTlT$M6M5orrncaHsn.z.RWRcL\/:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$uqDPdTlT$M6M5orrncaHsn.z.RWRcL\/:0:0:99999:7:::/g' /etc/shadow
+
+sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/video/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.vsean.net/openwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.80-1-9cfabe0504597f70c60bba6a0cfdc963/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.vsean.net/openwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+
+uci commit
+#不用重启network，源码自带
+exit 0
